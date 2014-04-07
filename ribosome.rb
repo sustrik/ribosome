f315c748938393d68d12f3ea343979937dd6e524
Martin Sustrik <sustrik@250bpm.com>
2014-04-07 09:26:01 +0200

    Move ribosome function to Ribosome module
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- ribosome.rb
+++ ribosome.rb
@@ -60,129 +60,141 @@ end
 
 rnahelpers = '
 
-def ____openroot____(name)
-    if(name[-4..-1] == ".xml")
-        require "rexml/document"
-        return REXML::Document.new(File.new(name)).root
-    elsif(name[-5..-1] == ".json")
-        require "rubygems"
-        require "json"
-        return JSON.parse(File.read(name))
-    else
-        $stderr.puts("input file must be either .json or .xml")
-        exit()
-    end
-end
+module Ribosome
 
-def ____write____(s)
-    $____stack____.last.push(s)
-end
-
-def ____close____()
-    for l in $____stack____.last
-        $____out____.write(l)
-    end
-    $____stack____ = [[]]
-    $____out____.write("\n");
-    if ($____outisafile____)
-        $____out____.close()
-    end
-end
-
-def ____expand____(s, b)
-
-    # Find all occurences of @{.
-    i = -1
-    while true
-        i = s.index(\'@{\', i + 1)
-        if(i == nil)
-            break;
-        end
-        j = i + 1
-
-        # Find corresponding }.
-        par = 0;
-        while true
-            if(s[j] == ?{)
-                par += 1
-            end
-            if(s[j] == ?})
-                par -= 1
-            end
-            if(par == 0)
-                break
-            end
-            j += 1
-        end
-
-        # Replace the expression with its value.
-        expr = s[i + 2..j - 1]
-        $____stack____.push([])
-        val = eval(expr, b)
-        top = $____stack____.pop()
-        if(top.empty?)
-
-            # Classic functions.
-            s[i..j] = val.to_s()
-            i += val.to_s().size
+    def Ribosome.openroot(name)
+        if(name[-4..-1] == ".xml")
+            require "rexml/document"
+            return REXML::Document.new(File.new(name)).root
+        elsif(name[-5..-1] == ".json")
+            require "rubygems"
+            require "json"
+            return JSON.parse(File.read(name))
         else
-
-            # Ribosome functions.
-            res = top.join()
-            s[i..j] = res
-            i += res.size
+            $stderr.puts("input file must be either .json or .xml")
+            exit()
         end
-        
     end
 
-    return s
-end
+    def Ribosome.write(s)
+        $stack.last.push(s)
+    end
 
-def ____adjust____(line, ws)
+    def Ribosome.close()
+        for l in $stack.last
+            $out.write(l)
+        end
+        $stack = [[]]
+        $out.write("\n");
+        if ($outisafile)
+            $out.close()
+        end
+    end
 
-    a = line.to_a()
+    def Ribosome.expand(s, b)
 
-    # Find top, bottom and left boundary of the block of text.
-    i = 0
-    top = -1
-    bottom = -1
-    left = -1
-    for l in a
-        if(!l.lstrip().empty?)
-            if(top == -1)
-                top = i;
+        # Find all occurences of @{.
+        i = -1
+        while true
+            i = s.index(\'@{\', i + 1)
+            if(i == nil)
+                break;
             end
-            bottom = i;
-            if (left == -1)
-                left = l.size() - l.lstrip().size()
+            j = i + 1
+
+            # Find corresponding }.
+            par = 0;
+            while true
+                if(s[j] == ?{)
+                    par += 1
+                end
+                if(s[j] == ?})
+                    par -= 1
+                end
+                if(par == 0)
+                    break
+                end
+                j += 1
+            end
+
+            # Replace the expression with its value.
+            expr = s[i + 2..j - 1]
+            $stack.push([])
+            val = eval(expr, b)
+            top = $stack.pop()
+            if(top.empty?)
+
+                # Classic functions.
+                s[i..j] = val.to_s()
+                i += val.to_s().size
             else
-                left = [left, l.size() - l.lstrip().size()].min
+
+                # Ribosome functions.
+                res = top.join()
+                s[i..j] = res
+                i += res.size
             end
+            
         end
-        i += 1
+
+        return s
     end
 
-    # Strip the top and bottom whitespace.
-    if(top == -1)
-        a = []
-    else
-        a = a[top..bottom]
+    def Ribosome.adjust(line, ws)
+
+        a = line.to_a()
+
+        # Find top, bottom and left boundary of the block of text.
+        i = 0
+        top = -1
+        bottom = -1
+        left = -1
+        for l in a
+            if(!l.lstrip().empty?)
+                if(top == -1)
+                    top = i;
+                end
+                bottom = i;
+                if (left == -1)
+                    left = l.size() - l.lstrip().size()
+                else
+                    left = [left, l.size() - l.lstrip().size()].min
+                end
+            end
+            i += 1
+        end
+
+        # Strip the top and bottom whitespace.
+        if(top == -1)
+            a = []
+        else
+            a = a[top..bottom]
+        end
+
+        # Strip the original whitespace from the left.
+        # Add the amount of whitespace specified in the argument.
+        for i in 0..a.size() - 1
+            a[i] = " " * ws + a[i][left..-1]
+        end
+
+        # Return the adjusted block. Strip off the trailing newline.
+        res = a.join
+        if(res[-1] == ?\n)
+            res = res[0..-2]
+        end
+        return res;
+
     end
 
-    # Strip the original whitespace from the left.
-    # Add the amount of whitespace specified in the argument.
-    for i in 0..a.size() - 1
-        a[i] = " " * ws + a[i][left..-1]
-    end
+    # Initialise the ribosome stack. Each level on the stack contains a block of
+    # text in the form of array of lines (strings).
+    $stack = [[]]
 
-    # Return the adjusted block. Strip off the trailing newline.
-    res = a.join
-    if(res[-1] == ?\n)
-        res = res[0..-2]
-    end
-    return res;
+    # Initialise output channel.
+    $out = $stdout
+    $outisafile = false
+
 end
-
 '
 
 ################################################################################
@@ -212,21 +224,13 @@ dna = File.open($dnafile, "r")
 rna = File.open(rnafile, "w")
 
 # Add RNA helper functions.
+
 rna.write(rnahelpers)
 
 # Open the input file.
 rna.write("if(ARGV.size > 0)\n")
-rna.write("    $root = ____openroot____(ARGV[0])\n")
+rna.write("    $root = Ribosome.openroot(ARGV[0])\n")
 rna.write("end\n\n")
-
-# Initialise the ribosome stack.
-# Each level on the stack contains a block of text in the form of
-# array of lines (strings).
-rna.write("$____stack____ = [[]]\n")
-
-# Initialise output channel.
-rna.write("$____out____ = $stdout\n")
-rna.write("$____outisafile____ = false\n")
 
 # Process the DNA file, line-by-line.
 while(line = dna.gets())
@@ -240,37 +244,46 @@ while(line = dna.gets())
     end
 
     # Lines starting with '!' are ribosome commands.
-    if(line[0] == ?!	)
-
-        # Check whether command is used in the outermost scope.
-        rna.write("if($____stack____.size > 1)\n")
-        rna.write("    $stderr.write(\"#{$dnafile}:#{$ln} - command used in a nested function\\n\")\n")
-        rna.write("    exit()\n")
-        rna.write("end\n")
+    if(line[0] == ?!)
 
         # Parse the arguments. This should probably be done in a more
         # sophisticated way in the future to allow arguments containing spaces.
         words = line[1..-1].split(/\s+/)
 
-        # >output redirects the output to a different file.
+        # !separate is used to insert separators between
+        # the iterations of a loop.
+        #if(words[0] == 'separate') {
+        #    rna.write("____first_#{$ln}____ = true\n")
+        #    line = dna.gets()
+        #    $ln += 1
+        #    next
+        #}
+
+        # Remaining commands can be used only in the outermost scope.
+        rna.write("if(Ribosome.$stack.size > 1)\n")
+        rna.write("    $stderr.write(\"#{$dnafile}:#{$ln} - command '#{words[0]}' used in a nested function\\n\")\n")
+        rna.write("    exit()\n")
+        rna.write("end\n")
+
+        # !output redirects the output to a different file.
         if(words[0] == 'output')
             if(words.size() != 2)
                 dnaerror("command 'output' expects one argument")
             end
-            rna.write("____close____()\n")
-            rna.write("____outisafile____ = true\n")
-            rna.write("$____out____ = File.open(#{words[1]}, 'w')\n")
+            rna.write("Ribosome.close()\n")
+            rna.write("Ribosome.$outisafile = true\n")
+            rna.write("Ribosome.$out = File.open(#{words[1]}, 'w')\n")
             next
         end
 
-        # >stdout redirects the output to stdout.
+        # !stdout redirects the output to stdout.
         if(words[0] == 'stdout')
             if(words.size() != 1)
                 dnaerror("command 'stdout' expects no arguments")
             end
-            rna.write("____close____()\n")
-            rna.write("____outisafile____ = false\n")
-            rna.write("$____out____ = $stdout\n")
+            rna.write("Ribosome.close()\n")
+            rna.write("Ribosome.$outisafile = false\n")
+            rna.write("Ribosome.$out = $stdout\n")
             next
         end
 
@@ -287,11 +300,11 @@ while(line = dna.gets())
         ws,line = ltrim(line)
 
         # Terminate the previous line.
-        rna.write("____write____(\"\\n\")\n")
+        rna.write("Ribosome.write(\"\\n\")\n")
 
         # 1. Expand any embedded control expressions.
         # 2. Trim any top, bottom or left whitespace.
-        rna.write("____write____(____adjust____(____expand____(#{line.inspect()}, binding()), #{ws}))\n")
+        rna.write("Ribosome.write(Ribosome.adjust(Ribosome.expand(#{line.inspect()}, binding()), #{ws}))\n")
 
         next
     end
@@ -304,7 +317,7 @@ while(line = dna.gets())
         ws,line = ltrim(line)
 
         # Expand any embedded control expressions.
-        rna.write("____write____(____expand____(#{line.inspect()}, binding()))\n")
+        rna.write("Ribosome.write(Ribosome::expand(#{line.inspect()}, binding()))\n")
 
         next
     end
@@ -315,7 +328,7 @@ while(line = dna.gets())
 end
 
 # Flush the output file.
-rna.write("____close____()\n\n")
+rna.write("Ribosome.close()\n\n")
 
 # Flush the RNA file.
 rna.close()
