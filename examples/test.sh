12718164e34c38ea89a2ab26be3856e2b31d800c
Martin Sustrik <sustrik@250bpm.com>
2014-04-15 22:58:13 +0200

    test.sh script added
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

---
+++ examples/test.sh
@@ -0,0 +1,12 @@
+cd errors
+../../ribosome errors.dna errors.json
+cd ..
+cd objects
+../../ribosome objects.dna complex.xml > complex.cpp
+cd ..
+cd toxml
+../../ribosome toxml.dna input.json > output.xml
+cd ..
+cd manyfiles
+../../ribosome manyfiles.dna manyfiles.json
+cd ..
